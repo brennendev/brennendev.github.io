cd1a71308760038ea67f07c70b6c8989f79e1409
bren <74978647+brennendev@users.noreply.github.com>
2023-05-30 14:41:03 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const xtElement = document.getElementById('developer');
+const contact = document.getElementById('contact');
+var text = 'Developer';
+
+const texts = ['Developer', 'ChatTriggers Enthusiast', 'Student', 'Malware Analyst', "Reaper Client On TOP"];
+var count = 0;
+
+setInterval(() => {
+    if (count >= texts.length) {
+        count = 0;
+    }
+    text = texts[count];
+    count++;
+  textElement.textContent = text;
+}, 3000);
+
+function redirect1() {
+    window.location.href = "https://github.com/brennendev/brennendev.github.io/"
+}
+
+function redirect2() {
+  window.location.href = "https://github.com/brennendev/htmlxsandos/"
+}
+
+
+
+
+function copyToClipboard(text) {
+    const input = document.createElement('input');
+    input.value = text;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand('copy');
+    document.body.removeChild(input);
+  }
+
+contact.addEventListener('click', () => {
+    alert('Copied to clipboard!');
+    copyToClipboard('coming soon... sorry');
+    event.preventDefault();
+})
